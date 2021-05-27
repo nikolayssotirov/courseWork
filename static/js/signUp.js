bcb3ded987436873b2eeded8eb982be317b363c7
nikolayssotirov <nikolayssotirov@gmail.com>
2021-05-27 16:31:17 +0300

    initial

---
+++ static/js/signUp.js
@@ -0,0 +1,27 @@
+if (typeof jQuery === "undefined") {
+    var script = document.createElement('script');
+    script.src = 'http://code.jquery.com/jquery-latest.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+//edit
+window.onload = function() {
+    $(function() {
+        $('#btnSignUp').click(function() {
+     
+            $.ajax({
+                url: '/signUp',
+                data: $('form').serialize(),
+                type: 'POST',
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+    });
+}
+
